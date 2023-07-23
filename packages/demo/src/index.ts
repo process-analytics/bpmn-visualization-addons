b2b10cf100bcd6f062d4152ec3d1c9054526d03b
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-07-23 22:31:21 +0200

    clean commented code

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -26,67 +26,10 @@ import {isBpmnArtifact} from "./bpmn-utils";
 // Instantiate BpmnVisualization, pass the container HTMLElement - present in index.html
 const bpmnVisualization = new BpmnVisualization({
   container: 'bpmn-container',
-  // navigation: {
-  //   enabled: true,
-  // },
 });
 // Load the BPMN diagram defined above
 bpmnVisualization.load(diagram, {fit: {type: FitType.Center, margin: 20}});
 const bpmnElementsRegistry = bpmnVisualization.bpmnElementsRegistry;
-
-
-// =====================================================================================================================
-// CODE OF THE DEMO TEMPLATE
-// =====================================================================================================================
-
-// Highlight tasks with CSS classes
-// bpmnElementsRegistry.addCssClasses(
-//   [
-//     'Activity_00vbm9s', // Record Goods Receipts
-//     'Activity_1t65hvk', // Create Purchase Order Item
-//   ],
-//   'bpmn-highlight',
-// );
-
-// Change the style of some elements using the "Update Style" API
-// bpmnElementsRegistry.updateStyle([
-//   'Activity_1u4jwkv', // Record Invoice Receipt
-//   'Flow_0lrixjg',
-//   'Gateway_0a68dfj',
-//   'Flow_1r9qd61',
-//   'Activity_083jf01', // Remove Payment Block
-// ], {stroke: {color: 'blue'}, fill: {color: 'lightblue'}});
-
-// Add overlays
-// const overlayConfiguration: Omit<Overlay, 'label'> = {
-//   position: 'top-center',
-//   style: {
-//     fill: {
-//       color: 'blue',
-//     },
-//     font: {
-//       color: 'white',
-//       size: 18,
-//     },
-//     stroke: {
-//       color: 'blue',
-//     },
-//   },
-// };
-// bpmnElementsRegistry.addOverlays('Activity_1u4jwkv',
-//   {
-//     ...overlayConfiguration,
-//     label: '12',
-//   });
-// bpmnElementsRegistry.addOverlays('Activity_083jf01',
-//   {
-//     ...overlayConfiguration,
-//     label: '19',
-//   });
-
-// =====================================================================================================================
-// NEW CODE
-// =====================================================================================================================
 
 const pathResolver = new PathResolver(bpmnElementsRegistry);
 
@@ -100,7 +43,6 @@ const registerSelectedBpmnElement = (id: string): boolean => {
 }
 
 const computedFlows: string[] = [];
-// const computedFullPath: string[] = [];
 
 function computePath() {
   console.info('compute path')
@@ -111,11 +53,7 @@ function computePath() {
 
   const visitedEdges = pathResolver.getVisitedEdges([...selectedBpmnElements]);
   computedFlows.push(...visitedEdges);
-  bpmnElementsRegistry.updateStyle(computedFlows,
-      {stroke: {color: 'orange', width: 3}});
-
-  // computedFullPath.length = 0;
-  // computedFullPath.push(...selectedBpmnElements, ...computedFlows);
+  bpmnElementsRegistry.updateStyle(computedFlows, {stroke: {color: 'orange', width: 3}});
 }
 
 function clearPath(): void {
@@ -124,9 +62,6 @@ function clearPath(): void {
   selectedBpmnElements.clear();
   computedFlows.length = 0;
 }
-
-
-
 
 const getAllFlowNodes = (): BpmnElement[] => bpmnElementsRegistry.getElementsByKinds(ShapeUtil.flowNodeKinds().filter(kind => !isBpmnArtifact(kind)));
 
@@ -169,12 +104,3 @@ const setupControlEventHandlers = () => {
 
 setupControlEventHandlers();
 setupBpmnElementEventHandlers();
-
-
-// =====================================================================================================================
-
-// // Display the bpmn-visualization version in the footer
-// const footer = document.querySelector<HTMLElement>('footer')!;
-// const version = bpmnVisualization.getVersion();
-// const versionAsString = `bpmn-visualization@${version.lib}`;
-// footer.textContent = `${versionAsString}`;

9d365220aa48e0c32765c1299f2e4b4521c2dc5c
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-08-20 14:07:57 +0200

    store jest configuration as ESM

--- packages/addons/jest.config.cjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  // testEnvironment: 'jsdom', // need to access to the browser objects
-};

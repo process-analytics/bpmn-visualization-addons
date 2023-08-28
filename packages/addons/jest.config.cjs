6ea76ac519a9305aa02c6709a355ae4aa5ccf754
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-08-28 16:06:57 +0200

    jest configuration
    # Conflicts:
    #	package-lock.json

---
+++ packages/addons/jest.config.cjs
@@ -0,0 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  // testEnvironment: 'jsdom', // need to access to the browser objects
+};

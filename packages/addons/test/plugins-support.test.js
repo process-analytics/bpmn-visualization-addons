3359d237322ecf952233d3f202be9d2170ee8f77
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-08-31 06:32:49 +0200

    chore: add infrastructure to write JavaScript tests with jest
    Jest is configured to run ESM tests.
    This requires to patch bpmn-visualization as its package.json lacks of ESM support in the latest
    available version. It will support it out of the box in a future version (at which point we can
    remove the patch from the project).
    
    Writing tests in TypeScript will be possible at a later date.

---
+++ packages/addons/test/plugins-support.test.js
@@ -0,0 +1,42 @@
+/*
+Copyright 2023 Bonitasoft S.A.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { expect, test } from '@jest/globals';
+import { BpmnVisualization } from '../dist';
+
+test('No errors when no plugins is set', () => {
+  const bpmnVisualization = new BpmnVisualization({ container: null });
+  expect(bpmnVisualization.getPlugin('unknown')).toBeUndefined();
+});
+
+class MyCustomPlugin {
+  constructor(bpmnVisualization) {
+    // do nothing
+  }
+  getPluginId() {
+    return 'custom-plugin';
+  }
+  doSomethingSpecial() {
+    return 5;
+  }
+}
+
+test('Load a plugin and retrieve it', () => {
+  const bpmnVisualization = new BpmnVisualization({ container: null, plugins: [MyCustomPlugin] });
+  const plugin = bpmnVisualization.getPlugin('custom-plugin');
+  expect(plugin).toBeDefined();
+  expect(plugin.doSomethingSpecial()).toBe(5);
+});

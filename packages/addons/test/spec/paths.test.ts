708a7ea6f053246ecc7c1463bbe7c34d192c23c7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-09-12 16:07:37 +0200

    feat: detect message flows with `PathResolver` (#120)
    The implementation already supports it. Add tests to validate it.

--- packages/addons/test/spec/paths.test.ts
+++ packages/addons/test/spec/paths.test.ts
@@ -14,14 +14,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { describe, expect, test } from '@jest/globals';
+import { beforeEach, describe, expect, test } from '@jest/globals';
 import { BpmnVisualization, PathResolver } from '../../src';
 import { readFileSync } from '../shared/io-utils';
 
 describe('getVisitedEdges', () => {
   const bpmnVisualization = new BpmnVisualization({ container: null! });
-  bpmnVisualization.load(readFileSync('./fixtures/bpmn/paths/simple.bpmn'));
   const pathResolver = new PathResolver(bpmnVisualization.bpmnElementsRegistry);
+
+  beforeEach(() => {
+    bpmnVisualization.load(readFileSync('./fixtures/bpmn/paths/simple.bpmn'));
+  });
 
   const ensureElementsExistInModel = (ids: string[]): void => {
     expect(bpmnVisualization.bpmnElementsRegistry.getModelElementsByIds(ids)).toHaveLength(ids.length);
@@ -101,5 +104,40 @@ describe('getVisitedEdges', () => {
       'Flow_Task_2_Task_3',
       'Flow_Task_3_Task_4', // from wrong incoming EndEvent_1
     ]);
+  });
+
+  describe('Detect message flows', () => {
+    beforeEach(() => {
+      bpmnVisualization.load(readFileSync('./fixtures/bpmn/paths/pools-and-message-flows.bpmn'));
+    });
+
+    test('From message end event', () => {
+      const ids = [
+        // in pool 1
+        'Task_1_1',
+        'EndEvent_Message_1',
+        // in pool 2
+        'BoundaryEvent_1',
+      ];
+      ensureElementsExistInModel(ids);
+      expect(pathResolver.getVisitedEdges(ids)).toEqual(['Flow_Task_1_1_EndEvent_Message_1', 'MessageFlow_1-2']);
+    });
+
+    test('From intermediate throw event', () => {
+      const ids = [
+        // in pool 2
+        'Gateway_1153l1f', // gateway after the intermediate throw message
+        'IntermediateEvent_Throw_Message_1',
+        // in pool 3
+        'IntermediateEvent_Catch_Message_1',
+        'Activity_1n685qk', // task 3.1 after the intermediate catch message
+      ];
+      ensureElementsExistInModel(ids);
+      expect(pathResolver.getVisitedEdges(ids)).toEqual([
+        'Flow_0i31twf', // sourceRef="IntermediateEvent_Throw_Message_1" targetRef="Gateway_1153l1f"
+        'MessageFlow_2-3',
+        'Flow_1eb0isl', // sourceRef="IntermediateEvent_Catch_Message_1" targetRef="Activity_1n685qk"
+      ]);
+    });
   });
 });

5349a0912fec95b6ee7314732cf7f3bcb5f7ac72
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-10-04 13:48:57 +0200

    feat: enable diagram navigation in the "Overlays" demo (#135)
    The BPMN navigation doesn't remove the style applied externally from
    mxGraph (to hide the overlays) by a call to `OverlaysPlugin.setVisible`
    The demo now enables the diagram navigation (with both Mouse event and
    buttons) to highlight the feature.

--- packages/demo/src/path-resolver.ts
+++ packages/demo/src/path-resolver.ts
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import './assets/shared.css';
 import './assets/path-resolver.css';
 import type { BpmnElement } from 'bpmn-visualization';
 import { BpmnVisualization, FitType } from 'bpmn-visualization';
